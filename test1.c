b10db9b587f370feb6a6ad0c7be5674181393353
WgZgYn <2049562063@qq.com>
2024-04-10 14:51:39 +0800

    add warning ignore

--- test1.c
+++ test1.c
@@ -1,5 +1,4 @@
 // #include "sort.hpp"
-#include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <threads.h>
--- utils.c
+++ utils.c
@@ -20,13 +20,18 @@ do {                          \
     *a = temp;                \
 } while(0)                    \
 
+// maybe useful in GCC compiler
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress"
+#pragma GCC diagnostic ignored "-Wunused-value"
 #define SWAP(a, b)          \
 do {                        \
     a && b && *a && *b;     \
     typeof(*a) temp = *b;   \
     *b = *a;                \
     *a = temp;              \
-} while(0)                  \
+} while(0)                  
+#pragma GCC diagnostic pop
 
 void gen_random(int nums[], int length, int upper) {
     srand(time(NULL));
